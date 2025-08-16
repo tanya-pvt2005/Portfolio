3bc9f0ac8d4175e0015190338affea64ff5b43b9
tanya-pvt2005 <tanya.pvt2005@gmail.com>
2025-08-16 21:27:25 +0530

    trophee commit

--- src/pages/Achieve/Achieve.jsx
+++ src/pages/Achieve/Achieve.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from "react";
+import styles from "./Achieve.module.css";
+import trophee from "../../assets/trophee.png"
 
 const Achieve = () => {
   return (
-    <div>
-      Achieve
-    </div>
-  )
-}
+    <div className={styles.container}>
+      <h2 className={styles.heading}></h2>
 
-export default Achieve
+      <div className={styles.wrapper}>
+        {/* Left side */}
+        <div className={styles.side}>
+          <div className={`${styles.box} ${styles.offsetTop}`}>Achievement 1</div>
+          <div className={styles.box}>Achievement 2</div>
+          <div className={styles.box}>Achievement 3</div>
+          <div className={`${styles.box} ${styles.offsetBottom}`}>Achievement 4</div>
+        </div>
+
+        {/* Circle */}
+        <div className={styles.circle}>
+          <img src = {trophee} height ='500'></img>
+        </div>
+
+        {/* Right side */}
+        <div className={styles.side}>
+          <div className={`${styles.box} ${styles.offsetTop}`}>Achievement 5</div>
+          <div className={styles.box}>Achievement 6</div>
+          <div className={styles.box}>Achievement 7</div>
+          <div className={`${styles.box} ${styles.offsetBottom}`}>Achievement 8</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Achieve;
