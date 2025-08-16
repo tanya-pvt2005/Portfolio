2b82ec06ad4c60123374b777c8ffa616289709f2
tanya-pvt2005 <tanya.pvt2005@gmail.com>
2025-08-16 20:58:14 +0530

    highlights

--- src/pages/Highlights/Highlights.jsx
+++ src/pages/Highlights/Highlights.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import styles from "./Highlights.module.css"
 
 const Highlights = () => {
   return (
-    <div>
-      
+    <div className={styles.main}>
+    <div className={styles.highlightsGrid}>
+      <div className={`${styles.highlight} ${styles.item1}`}></div>
+      <div className={`${styles.highlight} ${styles.item2}`}></div>
+      <div className={`${styles.highlight} ${styles.item3}`}></div>
+      <div className={`${styles.highlight} ${styles.item4}`}></div>
+      <div className={`${styles.highlight} ${styles.item5}`}></div>
+      <div className={`${styles.highlight} ${styles.item6}`}></div>
+   
+    </div>
     </div>
   )
 }
